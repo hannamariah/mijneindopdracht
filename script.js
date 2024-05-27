10c8b4ae33d001c01ea338f1f46a28fa45450aaf
hannamariah <hannaberhane@hotmail.com>
2024-05-27 11:43:30 +0200

    wijziging

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-console.log("hoi")
+const nameInput = document.getElementById("nameInput");
 
-const mijnGif = document.querySelector("#mijnGif");
-mijnGif.src = "gifs/giphypet2";
+const button = document.getElementById("button");
+const h2 = document.querySelector("h2");
+let tamagotchiName;
 
+function logInput() {
+    tamagotchiName = nameInput.value;
+    h2.textContent = "Hi, mijn naam is " + tamagotchiName;
+}
+
+
+button.addEventListener("click", logInput);
 let tamagotchi  = {
     naam: "Tamagotchi",
     honger: 50,
