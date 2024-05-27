7fac670116b344b4ab81c724c6e04621b80809da
hannamariah <hannaberhane@hotmail.com>
2024-05-27 10:23:40 +0200

    website toegevoegd

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("hoi")
+
+const mijnGif = document.querySelector("#mijnGif");
+mijnGif.src = "gifs/giphypet2";
+
+let tamagotchi  = {
+    naam: "Tamagotchi",
+    honger: 50,
+    blijheid: 50,
+    hygiëne: 50,
+    
+    toonStatus: function() {
+        document.getElementById("naam").innerHTML = tamagotchi.naam;
+        document.getElementById("status").innerHTML = `Trek: ${this.honger}<br>Blijheid: ${this.blijheid}<br>Hygiëne: ${this.hygiëne}`;
+    },
+    
+    voer: function() {
+        tamagotchi.honger -= 10;
+        tamagotchi.blijheid += 0;
+        tamagotchi.toonStatus();
+        document.getElementById("huisdier").classList.add("stuiter");
+        setTimeout(() => {
+            document.getElementById("huisdier").classList.remove("stuiter");
+        }, 600);
+    },
+    
+    speel: function() {
+        tamagotchi.honger += 5;
+        tamagotchi.blijheid += 10;
+        tamagotchi.hygiëne -= 5;
+        tamagotchi.toonStatus();
+        document.getElementById("huisdier").classList.add("stuiter");
+        setTimeout(() => {
+            document.getElementById("huisdier").classList.remove("stuiter");
+        }, 600);
+    },
+    
+    schoon: function() {
+        tamagotchi.hygiëne += 10;
+        tamagotchi.toonStatus();
+        document.getElementById("huisdier").classList.add("stuiter");
+        setTimeout(() => {
+            document.getElementById("huisdier").classList.remove("stuiter");
+        }, 600);
+    }
+};
+
+document.getElementById("voer").addEventListener("click", function() {
+    tamagotchi.voer();
+});
+
+document.getElementById("speel").addEventListener("click", function() {
+    tamagotchi.speel();
+});
+
+document.getElementById("schoon").addEventListener("click", function() {
+    tamagotchi.schoon();
+});
+
+tamagotchi.toonStatus();
+
+
+document.getElementById('startKnop').addEventListener('click', function() {
+    document.getElementById('mijnGif').src = 'gifs/mijnGif.gif'; // Herlaad de GIF om deze te herstarten
+});
+
+document.getElementById('stopKnop').addEventListener('click', function() {
+    document.getElementById('mijnGif').src = ''; // Stop de GIF door de src leeg te maken
+});
+
